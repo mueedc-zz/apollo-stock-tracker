eed7dc37e524c44b772a5979b75bae3a2f4ac442
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-01-16 20:37:16 -0500

    add email function

--- functions/email.js
+++ functions/email.js
@@ -1,2 +1,21 @@
-// add functionality to send emails
-// export function sendEmail (email) {}
+const nodemailer = require('nodemailer')
+
+export defailt function sendEmail (user, stock) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'apollostock@gmail.com' 
+      pass: process.env.GMAIL
+    }
+  })
+
+  const mailOptions = {
+    from: 'Apollo Stock <tracker@apollostock.io>'
+    to: user,
+    subject: `${stock} has dropped below your SPP!`
+    html: `<p>${stock} has dropped below your sell point percentage.
+      It might be time to think about selling.</p>` 
+  }
+
+  transporter.sendMail(mailOptions, (err, info) => err ? err : info)
+}
