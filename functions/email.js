b8c48879242678b91ba5cf55a8f0fd90383c1230
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-01-16 20:42:52 -0500

    fix typos

--- functions/email.js
+++ functions/email.js
@@ -1,21 +1,21 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 
-export defailt function sendEmail (user, stock) {
+export default function sendEmail(user, stock) {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'apollostock@gmail.com' 
-      pass: process.env.GMAIL
-    }
-  })
+      user: 'apollostock@gmail.com',
+      pass: process.env.GMAIL,
+    },
+  });
 
   const mailOptions = {
-    from: 'Apollo Stock <tracker@apollostock.io>'
+    from: 'Apollo Stock <tracker@apollostock.io>',
     to: user,
-    subject: `${stock} has dropped below your SPP!`
+    subject: `${stock} has dropped below your SPP!`,
     html: `<p>${stock} has dropped below your sell point percentage.
-      It might be time to think about selling.</p>` 
-  }
+      It might be time to think about selling.</p>`,
+  };
 
-  transporter.sendMail(mailOptions, (err, info) => err ? err : info)
+  transporter.sendMail(mailOptions, (err, info) => (err ? err : info));
 }
