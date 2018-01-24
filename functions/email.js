eb538677829ca4e71583e38a9d0c0a39fcee597d
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-01-24 11:32:21 -0500

    death of semi colons

--- functions/email.js
+++ functions/email.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
 export default function sendEmail(user, stock) {
   let transporter = nodemailer.createTransport({
@@ -7,7 +7,7 @@ export default function sendEmail(user, stock) {
       user: 'apollostock@gmail.com',
       pass: process.env.GMAIL,
     },
-  });
+  })
 
   const mailOptions = {
     from: 'Apollo Stock <tracker@apollostock.io>',
@@ -15,7 +15,7 @@ export default function sendEmail(user, stock) {
     subject: `${stock} has dropped below your SPP!`,
     html: `<p>${stock} has dropped below your sell point percentage.
       It might be time to think about selling.</p>`,
-  };
+  }
 
-  transporter.sendMail(mailOptions, (err, info) => (err ? err : info));
+  transporter.sendMail(mailOptions, (err, info) => (err ? err : info))
 }
