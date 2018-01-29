894173b59583e4b98ce59f867cd5544aa1f634b5
Mueed Chaudhry <mueed.chaudhry2@gmail.com>
2018-01-29 13:45:47 -0500

    reorg email function

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,8 @@
-const nodemailer = require('nodemailer')
 const axios = require('axios')
 
-export function getStock (symbol) {
+export default function getStock (symbol) {
   return axios.get(`/api/stock/${symbol}`)
     .then(res => res.data)
     .catch(err => console.error(err))
 }
 
-export function sendEmail(user, stock) {
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'apollostock@gmail.com',
-      pass: process.env.GMAIL,
-    },
-  })
-
-  const mailOptions = {
-    from: 'Apollo Stock <tracker@apollostock.io>',
-    to: user,
-    subject: `${stock} has dropped below your SPP!`,
-    html: `<p>${stock} has dropped below your sell point percentage.
-      It might be time to think about selling.</p>`,
-  }
-
-  transporter.sendMail(mailOptions, (err, info) => (err ? err : info))
-}
